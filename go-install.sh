dc96d956de1d07508cce3209a412035afaaedf64
Zhuojin Li <zhuojinl@usc.edu>
2020-05-20 22:05:29 -0700

    Add go-install.sh

---
+++ go-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install Golang
+sudo add-apt-repository ppa:longsleep/golang-backports -y
+sudo apt-get update
+sudo apt-get install golang-go -y
+
+# Install protobuf
+sudo apt-get install autoconf automake libtool curl make g++ unzip -y
+git clone https://github.com/google/protobuf.git
+cd protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure
+make -j16
+make check
+sudo make install
+sudo ldconfig
+cd -
+
+# Install gRPC as a go module
+export GO111MODULE=on
+go get google.golang.org/grpc
+
+# protoc plugin for Go
+go get github.com/golang/protobuf/protoc-gen-go
+export PATH="$PATH:$(go env GOPATH)/bin"
