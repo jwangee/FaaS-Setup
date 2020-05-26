fa46525f0986a77d6693eff1285a67508d30b647
Zhuojin Li <zhuojinl@usc.edu>
2020-05-25 17:49:32 -0700

    update

--- bess-install.sh
+++ bess-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt install make apt-transport-https ca-certificates g++ make pkg-config libunwind8-dev liblzma-dev zlib1g-dev libpcap-dev libssl-dev libnuma-dev git python python-pip python-scapy libgflags-dev libgoogle-glog-dev libgraph-easy-perl libgtest-dev libgrpc++-dev libprotobuf-dev libc-ares-dev libbenchmark-dev libgtest-dev protobuf-compiler-grpc
+sudo apt install make apt-transport-https ca-certificates g++ make pkg-config libunwind8-dev liblzma-dev zlib1g-dev libpcap-dev libssl-dev libnuma-dev git python python-pip python-scapy libgflags-dev libgoogle-glog-dev libgraph-easy-perl libgtest-dev libgrpc++-dev libprotobuf-dev libc-ares-dev libbenchmark-dev libgtest-dev protobuf-compiler-grpc -y
 # The following packages are needed to run bessctl
 pip install --user protobuf grpcio scapy
 
