741b735758bbbb10d038ad6237f5035e39542585
Jianfeng Wang <pkueewjf@gmail.com>
2020-08-30 14:15:09 -0700

    ryu: add a command for installing gflags
    This commit adds a command to install gflags to the Ryu controller
    server.

--- ryu-install.sh
+++ ryu-install.sh
@@ -4,7 +4,9 @@
 sudo apt update
 sudo apt install git python python-pip python-scapy -y
 sudo apt install gcc apt-transport-https ca-certificates libffi-dev libxml2-dev libxslt1-dev zlib1g-dev -y
-sudo apt install libunwind8-dev liblzma-dev libpcap-dev libssl-dev libgflags-dev libgoogle-glog-dev libgtest-dev libgrpc++-dev libprotobuf-dev libc-ares-dev libgtest-dev protobuf-compiler-grpc -y
+sudo apt install libunwind8-dev liblzma-dev libpcap-dev libssl-dev libgflags-dev libgoogle-glog-dev libgtest-dev libgrpc++-dev libprotobuf-dev libc-ares-dev libgtest-dev -y
+sudo apt install protobuf-compiler-grpc python-gflags -y
+
 # The following packages are needed to run bessctl
 sudo pip install protobuf grpcio grpcio-tools webob tinyrpc scapy
 
