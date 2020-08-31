193acd27b9ca1004897c032fb242a0d2d335cace
Jianfeng Wang <pkueewjf@gmail.com>
2020-08-31 00:46:06 -0700

    faas: add a isolcpus parameter to the default grub setting
    This commit adds a isolcpus parameter to the default grub setting. This
    change will block CPU 1-20 from normal user-level threads, and
    therefore make them available to FaaS-NFV system only.

--- setup.sh
+++ setup.sh
@@ -11,7 +11,8 @@ GRUB_BACKUP=/etc/default/grub_backup
 
 if [ ! -f ${GRUB_BACKUP} ]; then
   sudo cp /etc/default/grub ${GRUB_BACKUP}
-  sudo sed '/GRUB_CMDLINE_LINUX=/ s/"$/ iommu=pt intel_iommu=on"/' ${GRUB_BACKUP} | sudo tee /etc/default/grub > /dev/null
+  sudo sed '/GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ isolcpus=1-20"/' ${GRUB_BACKUP} | sudo tee /etc/default/grub > /dev/null
+  sudo sed '/GRUB_CMDLINE_LINUX=/ s/"$/ iommu=pt intel_iommu=on isolcpus=1-20"/' ${GRUB_BACKUP} | sudo tee /etc/default/grub > /dev/null
   sudo update-grub
   # TODO: CPUISO
   sudo reboot
