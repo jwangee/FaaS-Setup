8fff9fb8a4bd8233e959328aca6f892c87fc67d3
Jianfeng Wang <pkueewjf@gmail.com>
2020-09-24 23:10:31 -0700

    nf: add a script for installing dependencies of NF containers
    This commit adds a script that installs dependencies for compiling NF
    containers.

---
+++ nf-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This script installs all dependencies for compiling NF containers
+#
+
+# Install dependencies
+sudo apt update
+sudo apt install -y make apt-transport-https ca-certificates g++ make pkg-config
+sudo apt install -y git python python-pip python-scapy protobuf-compiler-grpc
+sudo apt install -y libunwind8-dev liblzma-dev zlib1g-dev libpcap-dev libssl-dev libnuma-dev
+sudo apt install -y libgflags-dev libgoogle-glog-dev libgraph-easy-perl libgtest-dev libgrpc++-dev libprotobuf-dev libc-ares-dev libbenchmark-dev libgtest-dev
+
+# The following packages are required to run bessctl
+sudo pip install protobuf grpcio scapy
+
+# Install Hiredis
+cd /local
+git clone http://github.com/redis/hiredis
+cd hiredis/
+make
+sudo make install
